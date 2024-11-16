05de1b4d0b21ae7b90b548067075910d4cbd9332
Yang98-git <76473646+Yang98-git@users.noreply.github.com>
2024-11-16 12:16:20 -0500

    deleted OrderHistory.java and OrderHistoryRepository.java

--- backend/e-shop-master/src/main/java/com/yang2/e_shop_master/dao/OrderHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yang2.e_shop_master.dao;
-
-import com.yang2.e_shop_master.entity.OrderHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Long> {
-}
