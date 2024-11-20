941165134232c709db8fc4e47a2bd6709f5620db
Shang Yang <yangvvva@gmail.com>
2024-11-20 02:09:45 -0500

    udpate screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
+import ShoppingCartScreen from "./screens/ShoppingCartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import AdminUserAccount from "./screens/AdminUserAccount";
 
 function App() {
   return (
@@ -10,7 +17,17 @@ function App() {
       <Header />
       <main>
         <Container>
-          <HomeScreen />
+          <Router>
+            <Routes>
+              <Route path="/" Component={HomeScreen} exact />
+              <Route path="/product/:id" Component={ProductScreen} />
+              <Route path="/product/:id" Component={ShoppingCartScreen} />
+              <Route path="/login" Component={LoginScreen} />
+              <Route path="/register" Component={RegisterScreen} />
+              <Route path="/payment" Component={PaymentScreen} />
+              <Route path="/admin/users" Component={AdminUserAccount} />
+            </Routes>
+          </Router>
         </Container>
       </main>
       <Footer />
